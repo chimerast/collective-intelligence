f7fb037fea5d5c60a1bc0b7977fd126cf1cca4c0
Hideyuki TAKEUCHI <webmaster@chimera.st>
2011-09-16 04:06:31 +0900

    日経225株価推移を解析してみた

--- src/main/scala/chapter3/Chapter3.scala
+++ src/main/scala/chapter3/Chapter3.scala
@@ -10,6 +10,8 @@ import scala.collection.BitSet
 object Chapter3 extends App {
   implicit val codec = Codec.string2codec("UTF-8")
 
+  val input = "nikkei.txt"
+
   def readfile(filename: String): (Array[String], Array[String], Array[Array[Double]]) = {
     val lines = Source.fromFile(filename).getLines
 
@@ -25,19 +27,19 @@ object Chapter3 extends App {
   }
 
   section("3.3 階層的クラスタリング") {
-    val (blognames, words, data) = readfile("blogdata.txt")
+    val (blognames, words, data) = readfile(input)
     val clust = BiCluster.hcluster(data)
     BiCluster.printclust(clust, Some(blognames))
   }
 
   section("3.4 デンドログラムを描く") {
-    val (blognames, words, data) = readfile("blogdata.txt")
+    val (blognames, words, data) = readfile(input)
     val clust = BiCluster.hcluster(data)
     Dendrogram(clust, blognames)
   }
 
   section("3.6 K平均法によるクラスタリング") {
-    val (blognames, words, data) = readfile("blogdata.txt")
+    val (blognames, words, data) = readfile(input)
     val kclust = BiCluster.kcluster(data, k = 10)
     kclust.zipWithIndex.map {
       case (k, i) => println("k[%d]: %s".format(i, k.map(blognames).mkString(", ")))
@@ -51,7 +53,7 @@ object Chapter3 extends App {
   }
 
   section("3.8 データを2次元で見る") {
-    val (blognames, words, data) = readfile("blogdata.txt")
+    val (blognames, words, data) = readfile(input)
     val coords = MDScaling.scaledown(data)
     MDScaling(coords, blognames)
   }
--- src/main/scala/chapter3/Nikkei225.scala
+++ src/main/scala/chapter3/Nikkei225.scala
@@ -1,14 +1,91 @@
 package chapter3
 
 import java.net._
+import java.sql._
+import java.io._
 import util._
 import util.HtmlScraper._
 import net.htmlparser.jericho._
 
+import org.joda.time._
+
+import org.scalaquery.session._
+import org.scalaquery.session.Database.threadLocalSession
+
+import org.scalaquery.ql._
+import org.scalaquery.ql.TypeMapper._
+import org.scalaquery.ql.extended.{ ExtendedTable => Table }
+import org.scalaquery.ql.extended.MySQLDriver.Implicit._
+
 object Nikkei225 extends App {
-  val html = HtmlScraper("http://www3.nikkei.co.jp/nkave/about/225_list.cfm")
+  val outfile = "nikkei.txt"
+
+  val url = "http://www3.nikkei.co.jp/nkave/about/225_list.cfm"
   val xpath = "//tr[@bgcolor='#8A4500' or @bgcolor='#FFF5DE']/td[1]/span[1]/text()"
 
-  html.eval(xpath).map(_.toString.toInt).sortBy(i => i).foreach(println)
+  val nikkei225 = DiskStore.load(HtmlScraper(url).eval(xpath).map(_.toString.toInt).sortBy(i => i), url)
 
+  def toSql(localDate: LocalDate) = new Date(localDate.toDate.getTime)
+
+  val db = Database.forURL("jdbc:mysql://db/speeda?user=system&password=system", driver = "com.mysql.jdbc.Driver")
+
+  val corpName = db withSession {
+    val q = for (c <- CorpBasis) yield c.*
+    q.list.toMap
+  }
+
+  val start = new LocalDate(2011, 6, 1)
+  val end = new LocalDate(2011, 7, 1).minusDays(1)
+
+  val map = (for (code <- nikkei225) yield {
+    val prices = db withSession {
+      val q = for (
+        s <- SharePrice if s.corpId === code
+          && s.marketId === SharePrice.TOSHO
+          && s.chartType === SharePrice.DAILY
+          && s.infoDate.between(toSql(start), toSql(end));
+        _ <- Query orderBy s.infoDate
+      ) yield s.infoDate ~ s.price
+
+      q.list
+    }
+
+    val points = (prices zip prices.drop(1)).map {
+      case ((_, prev), (date, curr)) =>
+        (date -> (curr - prev) / prev * 100.0)
+    }.toMap.withDefaultValue(0.0)
+
+    (code -> points)
+  }).toMap
+
+  using(new PrintWriter(outfile, "UTF-8")) { out =>
+    val days = map(9501).keys.toList.sortBy(_.getTime)
+
+    out.print("Nikkei225")
+    days.foreach(day => out.print("\t%s".format(day)))
+    out.println()
+    for ((code, points) <- map) {
+      out.print(corpName(code))
+      days.foreach(day => out.print("\t%.2f" format points(day)))
+      out.println()
+    }
+  }
+}
+
+object CorpBasis extends Table[(Int, String)]("corp_bss") {
+  def corpId = column[Int]("corp_id")
+  def name = column[String]("corp_imfrml_name_vch")
+  def * = corpId ~ name
+}
+
+object SharePrice extends Table[(Int, Int, Int, Date, Double)]("shr_price") {
+  def corpId = column[Int]("corp_id")
+  def marketId = column[Int]("mkt_id")
+  def chartType = column[Int]("cndl_chart_typ")
+  def infoDate = column[Date]("info_date")
+  def price = column[Double]("close_price_num")
+  def * = corpId ~ marketId ~ chartType ~ infoDate ~ price
+
+  val TOSHO = 1
+  val DAILY = 1
 }
