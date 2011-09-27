817635b2eb67c279bd63fefb22e0c59ccf904449
Hideyuki TAKEUCHI <webmaster@chimera.st>
2011-09-28 00:59:19 +0900

    5.9まで

--- src/main/scala/chapter5/Chapter5.scala
+++ src/main/scala/chapter5/Chapter5.scala
@@ -39,12 +39,12 @@ object Chapter5 extends App {
     hour.toInt * 60 + minute.toInt
   }
 
-  def printScedule(r: Array[Int]): Unit = {
-    for (d <- 0 until r.size / 2) {
+  def printScedule(vec: Array[Int]): Unit = {
+    for (d <- 0 until vec.size / 2) {
       val name = people(d)._1
       val origin = people(d)._2
-      val out = flights((origin, destination))(r(d * 2))
-      val ret = flights((destination, origin))(r(d * 2 + 1))
+      val out = flights((origin, destination))(vec(d * 2))
+      val ret = flights((destination, origin))(vec(d * 2 + 1))
       printf("%10s%10s %5s-%5s $%3d %5s-%5s $%3d\n",
         name, origin, out._1, out._2, out._3, ret._1, ret._2, ret._3)
     }
@@ -56,16 +56,16 @@ object Chapter5 extends App {
     printScedule(s)
   }
 
-  def scheduleCost(r: Array[Int]): Int = {
+  def scheduleCost(vec: Array[Int]): Int = {
     var totalprice = 0
     var latestarrival = 0
     var earliestdep = 24 * 60
 
-    for (d <- 0 until r.size / 2) {
+    for (d <- 0 until vec.size / 2) {
       // 行きと帰りのフライトを得る
       val origin = people(d)._2
-      val out = flights((origin, destination))(r(d * 2))
-      val ret = flights((destination, origin))(r(d * 2 + 1))
+      val out = flights((origin, destination))(vec(d * 2))
+      val ret = flights((destination, origin))(vec(d * 2 + 1))
 
       // 運賃総額total pliceは出立便と帰宅便すべての運賃
       totalprice += out._3
@@ -79,10 +79,10 @@ object Chapter5 extends App {
     // 最後の人が到着するまで全員空港で待機。
     // 帰りも空港にみんなできて自分の便を待たねばならない。
     var totalwait = 0
-    for (d <- 0 until r.size / 2) {
+    for (d <- 0 until vec.size / 2) {
       val origin = people(d)._2
-      val out = flights((origin, destination))(r(d * 2))
-      val ret = flights((destination, origin))(r(d * 2 + 1))
+      val out = flights((origin, destination))(vec(d * 2))
+      val ret = flights((destination, origin))(vec(d * 2 + 1))
 
       totalwait += latestarrival - getMinutes(out._2)
       totalwait += getMinutes(ret._1) - earliestdep
@@ -102,13 +102,17 @@ object Chapter5 extends App {
 
   type Cost = (Array[Int]) => Int
 
+  def generateRandom(domain: Array[(Int, Int)]): Array[Int] = {
+    Array.tabulate(domain.size)(i => domain(i)._1 + Random.nextInt(domain(i)._2 - domain(i)._1))
+  }
+
   def randomOptimize(domain: Array[(Int, Int)], costf: Cost): Array[Int] = {
     var best = Int.MaxValue
     var bestr = Array[Int]()
 
     for (i <- 0 until 1000) {
       // 無作為解の生成
-      val r = Array.tabulate(domain.size)(i => domain(i)._1 + Random.nextInt(domain(i)._2 - domain(i)._1))
+      val r = generateRandom(domain)
 
       // コストの取得
       val cost = costf(r)
@@ -124,7 +128,7 @@ object Chapter5 extends App {
   }
 
   section("5.4 ランダムサーチ") {
-    val domain = Array.fill(people.size * 2)((0, 9))
+    val domain = Array.fill(people.size * 2)((0, 10))
     val s = randomOptimize(domain, scheduleCost)
     println(scheduleCost(s))
     printScedule(s)
@@ -132,7 +136,7 @@ object Chapter5 extends App {
 
   def hillclimbOptimize(domain: Array[(Int, Int)], costf: Cost): Array[Int] = {
     // 無作為解の生成
-    var sol = Array.tabulate(domain.size)(i => domain(i)._1 + Random.nextInt(domain(i)._2 - domain(i)._1))
+    var sol = generateRandom(domain)
 
     val neighbors = ArrayBuffer[Array[Int]]()
     breakable {
@@ -165,7 +169,7 @@ object Chapter5 extends App {
   }
 
   section("5.5 ヒルクライム") {
-    val domain = Array.fill(people.size * 2)((0, 9))
+    val domain = Array.fill(people.size * 2)((0, 10))
     val s = hillclimbOptimize(domain, scheduleCost)
     println(scheduleCost(s))
     printScedule(s)
@@ -173,7 +177,7 @@ object Chapter5 extends App {
 
   def annealingOptimize(domain: Array[(Int, Int)], costf: Cost, T: Double = 10000.0, cool: Double = 0.95, step: Int = 1): Array[Int] = {
     // ランダムな値で解を初期化
-    var sol = Array.tabulate(domain.size)(i => domain(i)._1 + Random.nextInt(domain(i)._2 - domain(i)._1))
+    var sol = generateRandom(domain)
 
     var t = T
     while (t > 0.1) {
@@ -205,9 +209,149 @@ object Chapter5 extends App {
   }
 
   section("5.6 模擬アニーリング") {
-    val domain = Array.fill(people.size * 2)((0, 9))
+    val domain = Array.fill(people.size * 2)((0, 10))
     val s = annealingOptimize(domain, scheduleCost)
     println(scheduleCost(s))
     printScedule(s)
+  }
+
+  def geneticOptimize(domain: Array[(Int, Int)], costf: Cost, popsize: Int = 50, step: Int = 1, mutprob: Double = 0.2, elite: Double = 0.2, maxiter: Int = 100): Array[Int] = {
+    // 突然変異の操作
+    def mutate(vec: Array[Int]): Array[Int] = {
+      val i = Random.nextInt(domain.size)
+      if (random < 0.5 && vec(i) > domain(i)._1) {
+        vec.take(i) ++ Array(vec(i) - step) ++ vec.drop(i + 1)
+      } else if (vec(i) < domain(i)._2 - 1) {
+        vec.take(i) ++ Array(vec(i) + step) ++ vec.drop(i + 1)
+      } else {
+        vec
+      }
+    }
+
+    // 交叉の操作
+    def crossover(r1: Array[Int], r2: Array[Int]): Array[Int] = {
+      val i = Random.nextInt(domain.size - 2) + 1
+      r1.take(i) ++ r2.drop(i)
+    }
+
+    // 初期個体群の構築
+    var pop = ArrayBuffer((0 until popsize).map(_ => generateRandom(domain)): _*)
+
+    // 各世代の勝者数は？
+    val topelite = (elite * popsize).toInt
+
+    var ret = Array[Int]()
+
+    // Main loop
+    for (_ <- 0 until maxiter) {
+      val scores = pop.map(v => (costf(v), v)).sortBy(_._1)
+      val ranked = scores.map(_._2)
+
+      // まず純粋な勝者
+      pop = ranked.take(topelite)
+
+      // 勝者に突然変異や交配を行ったものを追加
+      while (pop.size < popsize) {
+        if (random < mutprob) {
+          // 突然変異
+          val c = ranked(Random.nextInt(topelite))
+          pop.append(mutate(c))
+        } else {
+          // 交叉
+          val c1 = ranked(Random.nextInt(topelite))
+          val c2 = ranked(Random.nextInt(topelite))
+          pop.append(crossover(c1, c2))
+        }
+      }
+
+      // println(scores(0)._1)
+      ret = scores(0)._2
+    }
+
+    ret
+  }
+
+  section("5.7 遺伝アルゴリズム") {
+    val domain = Array.fill(people.size * 2)((0, 10))
+    val s = geneticOptimize(domain, scheduleCost)
+    println(scheduleCost(s))
+    printScedule(s)
+  }
+
+  section("5.9 嗜好への最適化") {
+    // 寮、それぞれ空きが二つある
+    val dorms = Array("Zeus", "Athena", "Hercules", "Bacchus", "Pluto")
+
+    // 人、第１、第２希望を伴う
+    val prefs = Array(
+      ("Toby", ("Bacchus", "Hercules")),
+      ("Steve", ("Zeus", "Pluto")),
+      ("Karen", ("Athena", "Zeus")),
+      ("Sarah", ("Zeus", "Pluto")),
+      ("Dave", ("Athena", "Bacchus")),
+      ("Jeff", ("Hercules", "Pluto")),
+      ("Fred", ("Pluto", "Athena")),
+      ("Suzie", ("Bacchus", "Hercules")),
+      ("Laura", ("Bacchus", "Hercules")),
+      ("Neil", ("Hercules", "Athena")))
+
+    val domain = Array.tabulate(dorms.size * 2)(i => (0, i + 1)).reverse
+
+    def printSolution(vec: Array[Int]): Unit = {
+      // 各寮につきスロットを二つずつ生成
+      val slots = ArrayBuffer(dorms.indices.map(i => Array(i, i)).flatten: _*)
+
+      // 学生割り当て結果にループをかける
+      for (i <- vec.indices) {
+        val x = vec(i)
+
+        // 残っているスロットから一つ選ぶ
+        val dorm = dorms(slots(x))
+        // 学生とその割当先の寮を表示
+        println("%s %s" format (prefs(i)._1, dorm))
+        // このスロットを削除
+        slots.remove(x)
+      }
+    }
+
+    subsection("表示")
+    printSolution(Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+    def dormcost(vec: Array[Int]): Int = {
+      var cost = 0
+      // スロットのリストを生成
+      val slots = ArrayBuffer(dorms.indices.map(i => Array(i, i)).flatten: _*)
+
+      // 学生にループをかける
+      for (i <- vec.indices) {
+        val x = vec(i)
+        val dorm = dorms(slots(x))
+        val pref = prefs(i)._2
+        // 第１希望のコスト０、第２希望のコスト１、リストに無ければコスト３
+        cost += (dorm match {
+          case pref._1 => 0
+          case pref._2 => 1
+          case _ => 3
+        })
+
+        slots.remove(x)
+      }
+
+      cost
+    }
+
+    subsection("randomOptimize"); {
+      val s = randomOptimize(domain, dormcost)
+      println(dormcost(s))
+    }
+    subsection("hillclimbOptimize"); {
+      val s = hillclimbOptimize(domain, dormcost)
+      println(dormcost(s))
+    }
+    subsection("geneticOptimize"); {
+      val s = geneticOptimize(domain, dormcost)
+      println(dormcost(s))
+      printSolution(s)
+    }
   }
 }
