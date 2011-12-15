6ed8778f554187ce20fefeebd3ca79b2203a58f8
Hideyuki TAKEUCHI <webmaster@chimera.st>
2011-12-16 03:12:28 +0900

    東証１部上場企業でNMF

--- src/main/scala/chapter10/NMF.scala
+++ src/main/scala/chapter10/NMF.scala
@@ -2,20 +2,102 @@ package chapter10
 
 import scala.util.control.Breaks._
 
-import org.apache.commons.math.linear.Array2DRowRealMatrix
-import org.apache.commons.math.linear.RealMatrix
+import java.net._
+import java.sql.Date
+import java.io._
+import util._
+import util.HtmlScraper._
+import net.htmlparser.jericho._
 
-object NMF extends {
+import org.apache.commons.math.linear._
 
-  def main(args: Array[String]) {
-    val a = Array(Array(2.0, 0.0, 3.0, 0.0), Array(0.0, 2.0, 0.0, 1.0), Array(0.0, 0.0, 1.0, 1.0))
-    val (w, h) = factorize(new Array2DRowRealMatrix(a), 2, 100)
+import org.joda.time._
 
-    println(w.multiply(h))
+import org.scalaquery.session.Database.threadLocalSession
+import org.scalaquery.session._
+
+import org.scalaquery.ql.TypeMapper._
+import org.scalaquery.ql.extended.MySQLDriver.Implicit._
+import org.scalaquery.ql.extended.{ ExtendedTable => Table }
+import org.scalaquery.ql._
+
+import org.joda.time._
+
+object NMF extends App {
+  val outfile = "nikkei.txt"
+
+  val url = "http://www3.nikkei.co.jp/nkave/about/225_list.cfm"
+  val xpath = "//tr[@bgcolor='#F0E7D1' or @bgcolor='#FFF5DE']/td[1]/span[1]/text()"
+
+  val nikkei225 = DiskStore.load(HtmlScraper(url).eval(xpath).map(_.toString.toInt).sorted, url)
+
+  def toSql(localDate: LocalDate) = new Date(localDate.toDate.getTime)
+
+  val db = Database.forURL("jdbc:mysql://db/speeda?user=system&password=system", driver = "com.mysql.jdbc.Driver")
+
+  val corps = db withSession {
+    val q = for (
+      c <- CorpBasis if c.listed === 1
+        && c.corp === 1
+        && c.tosho === 11
+    ) yield c.corpId ~ c.name
+
+    q.toMap
   }
 
-  def difcost(a: RealMatrix, b: RealMatrix): Double =
+  val start = new LocalDate(2011, 1, 1)
+  val end = new LocalDate(2011, 7, 1).minusDays(1)
+
+  val codes = corps.keys.toList.sorted
+
+  def loadData() = {
+    (for (code <- codes) yield {
+      val volumes = db withSession {
+        val q = for (
+          s <- SharePrice if s.corpId === code
+            && s.marketId === SharePrice.TOSHO
+            && s.chartType === SharePrice.DAILY
+            && s.infoDate.between(toSql(start), toSql(end));
+          _ <- Query orderBy s.infoDate
+        ) yield s.infoDate ~ s.volume
+
+        q.toMap
+      }
+
+      println(code)
+      (code -> volumes)
+    }).toMap
+  }
+
+  val map = DiskStore.load(loadData, "stockdata")
+
+  val days = map.first._2.keys.toList
+
+  val array = Array.tabulate(codes.size, days.size) {
+    case (corp, day) =>
+      val a = (map(codes(corp)).withDefaultValue(1.0))(days(day))
+      if (a.isInfinite) println("NG")
+      a
+  }
+
+  val matrix = new Array2DRowRealMatrix(array)
+
+  val pc = 40
+  val (w, h) = factorize(matrix, pc, 100)
+
+  println
+
+  for (i <- 0 until pc) {
+    (codes.map(corps) zip w.getColumn(i)).sortBy(-_._2).take(5).foreach(print)
+    println
+    (days zip h.getRow(i)).sortBy(-_._2).take(5).foreach(print)
+    println
+    println
+  }
+
+  def difcost(a: RealMatrix, b: RealMatrix): Double = {
     a.subtract(b).getData().map(_.map(math.abs).sum).sum
+  }
 
   def factorize(v: RealMatrix, pc: Int = 10, iter: Int = 50) = {
     val ic = v.getRowDimension
@@ -50,4 +132,26 @@ object NMF extends {
 
     (w, h)
   }
+}
+
+object CorpBasis extends Table[(Int, String, Int, Int, Int)]("corp_bss") {
+  def corpId = column[Int]("corp_id")
+  def name = column[String]("corp_imfrml_name_vch")
+  def listed = column[Int]("list_sgn")
+  def corp = column[Int]("corp_sgn")
+  def tosho = column[Int]("tosho_id")
+  def * = corpId ~ name ~ listed ~ corp ~ tosho
+}
+
+object SharePrice extends Table[(Int, Int, Int, Date, Double, Double)]("shr_price") {
+  def corpId = column[Int]("corp_id")
+  def marketId = column[Int]("mkt_id")
+  def chartType = column[Int]("cndl_chart_typ")
+  def infoDate = column[Date]("info_date")
+  def price = column[Double]("close_price_num")
+  def volume = column[Double]("trde_volm_num")
+  def * = corpId ~ marketId ~ chartType ~ infoDate ~ price ~ volume
+
+  val TOSHO = 1
+  val DAILY = 1
 }
